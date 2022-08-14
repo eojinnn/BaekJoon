f81632c3a5fae09bb140a378b6a6f7bd502c512c
eojinnn <98944019+eojinnn@users.noreply.github.com>
2022-08-14 19:01:09 +0900

    Create 1000.cpp
    백준 1000번문제

---
+++ 1000.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int sum(int x,int y);
+int main(){
+    int a,b,result;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    result = sum(a,b);
+    printf("%d",result);
+}
+int sum(int x,int y){
+    return x+y;
+}
