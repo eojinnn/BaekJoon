836fc954d89cb0945883471a19c81fd3ef856086
eojinnn <ur.awesome.jin@gmail.com>
2023-01-29 17:33:52 +0900

    solve 2563 using Python ver.2 2dim array 152ms

--- 2dim_array/2563.py
+++ 2dim_array/2563.py
@@ -1,25 +1,34 @@
-"""cnt = int(input())
-Page = [x for x in range(10000)] #가로부터 1~100
-# 전체 페이지
-for i in range(cnt):
-    Rec = []
-    R, L = map(int,input().split())
-    #전체 페이지에서 채워진 부분 제외
-    for j in range(L,L+11):
-        Rec.append(x for x in range(100*j+R,100*j+R+11))
-    for x in Page:
-        if x in Rec:
-            Page.remove(x)"""
+# cnt = int(input())
+# Rec = []
+# for i in range(cnt):
+#     R,L = map(int,input().split())
+#     for j in range(L,L+10):
+#         for k in range(100*j+R,100*j+R+10):
+#             if k not in Rec:
+#                 Rec.append(k)
+# print(len(Rec))
+# import numpy as np
+# li = np.array([])
+# cnt = int(input())
+# for i in range(cnt):
+#     A, B = map(int,input().split())
+#     for k in range(B,B+10):
+#         for j in range(A,A+10):
+#             if [j,k] not in li:
+#                 li = np.vstack(li,np.array([j,k]))
+# print(li)
 
+li = []
 cnt = int(input())
-Rec = []
 for i in range(cnt):
-    R,L = map(int,input().split())
-    for j in range(L,L+10):
-        for k in range(100*j+R,100*j+R+10):
-            if k not in Rec:
-                Rec.append(k)
-print(len(Rec))
+    x, y = map(int,input().split())
+    for j in range(x,x+10):
+        for k in range(y,y+10):
+            li.append(list([j,k]))
+    nli = list(set([tuple(coordinates) for coordinates in li]))
+print(len(nli))
+    
+
 
 
     
